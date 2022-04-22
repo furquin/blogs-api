1e6c92250c13e34a518bfe4dfcffced879c69702
furquin <laert.ff@gmail.com>
2022-04-22 00:06:03 -0300

    feat: cria middlewares para validar atualização e usuário

--- middlewares/validatePost.js
+++ middlewares/validatePost.js
@@ -1,5 +1,7 @@
+const jwt = require('jsonwebtoken');
 const { getCategoriesById } = require('../services/categories');
 const { getByIdPost } = require('../services/posts');
+const { getUserByEmail } = require('../services/user');
 
 const validPost = (req, res, next) => {
     const { title, content } = req.body;
@@ -53,8 +55,41 @@ const postExits = async (req, res, next) => {
 }
 };
 
+const validUpdate = async (req, res, next) => {
+    const { categoryIds } = req.body;
+    
+    try {
+        if (categoryIds) {
+            return res.status(400).json({ message: 'Categories cannot be edited' });
+        }
+        next();
+    } catch (e) {
+        next(e);
+    }
+};
+
+const validUser = async (req, res, next) => {
+    const { id } = req.params;
+    const { authorization } = req.headers;
+    const { email } = jwt.decode(authorization);
+    try {
+        const { dataValues } = await getUserByEmail(email);
+        const { userId } = await getByIdPost(id);
+
+            if (userId !== dataValues.id) {
+                return res.status(401).json({ message: 'Unauthorized user' });
+            }
+
+        next();
+    } catch (e) {
+        next(e);
+    }
+};
+
 module.exports = {
     validPost,
     validCategory,
     postExits,
+    validUpdate,
+    validUser,
 };
