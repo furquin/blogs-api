2d834c45e4ee9e7d40159bdef5cd151a27dbda11
furquin <laert.ff@gmail.com>
2022-04-25 17:53:24 -0300

    fix: atualiza dependencias e corrige erro

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ module.exports = {
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'database_test',
+    database: 'blogs_api',
     host: process.env.DB_HOSTNAME,
     dialect: 'mysql',
   },
